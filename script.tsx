570e87d5761b25a686b57d22c7bde0fca818bf19
马啸天 <12233035+ma-xiaotiantian@user.noreply.gitee.com>
2026-06-04 17:24:39 +0800

    feat: 材质

--- script.tsx
+++ script.tsx
@@ -1,102 +1,15 @@
 import * as THREE from 'three'
-// import './style.css' // 不知道为什么会报错，但是在src文件夹下引入style.css就不会报错，可能是因为vite的配置问题，反正有效果，暂时先这样引入吧
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 const canvas = document.querySelector('.webgl') as HTMLCanvasElement
+const clock = new THREE.Clock()
 
 const size = {
   width: window.innerWidth,
   height: window.innerHeight
 }
 
-/**
-<<<<<<< HEAD
- *  材质
- */
-
-// const image = new Image()
-// const texture = new THREE.Texture(image)
-// // 如果直接使用TextureLoader加载纹理，可能会遇到跨域问题，尤其是在本地开发环境中。
-// // 使用Image对象加载纹理可以绕过这个问题，因为它会遵循浏览器的同源策略。
-// image.onload = () => {
-//   texture.needsUpdate = true
-//   // console.log('image loaded')
-// }
-
-// image.src = '/src/static/textures/door/color.jpg'
-// const textureLoader = new THREE.TextureLoader()
-
-// 纹理加载器，使用纹理加载器加载纹理时，浏览器会自动处理跨域问题，尤其是在本地开发环境中。
-const loadingManager = new THREE.LoadingManager()
-// loadingManager.onStart = () => {
-//   console.log('开始加载')
-// }
-// loadingManager.onLoad = () => {
-//   console.log('加载完成')
-// }
-// loadingManager.onProgress = () => {
-//   console.log('加载中')
-// }
-// 纹理加载器，一个纹理加载器可以加载多个纹理
-const textureLoader = new THREE.TextureLoader(loadingManager)
-
-// 加载门板基础颜色贴图，决定物体本身固有色
-const texture = textureLoader.load('/src/static/textures/minecraft.png')
-// 加载透明通道贴图，黑白控制模型透明与镂空区域
-// const alphaTexture = textureLoader.load('/src/static/textures/door/alpha.jpg')
-// // 加载高度凹凸贴图，配合位移修改模型表面凸起凹陷细节
-// const heightTexture = textureLoader.load('/src/static/textures/door/height.jpg')
-// // 加载法线贴图，在不增加面数前提下模拟表面细微凹凸光影
-// const normalTexture = textureLoader.load('/src/static/textures/door/normal.jpg')
-// // 加载环境光遮蔽贴图，模拟缝隙角落阴暗阴影，增强立体感
-// const ambientOcclusionTexture = textureLoader.load('/src/static/textures/door/ambientOcclusion.jpg')
-// // 加载金属度贴图，黑白区分材质金属/非金属属性
-// const metalnessTexture = textureLoader.load('/src/static/textures/door/metalness.jpg')
-// // 加载粗糙度贴图，控制表面光滑/粗糙程度，影响反光强弱
-// const roughnessTexture = textureLoader.load('/src/static/textures/door/roughness.jpg')
-
-// 设置纹理重复和包裹模式，默认情况下，纹理坐标在0到1之间，如果超出这个范围，纹理会被拉伸或者截断。
-// 通过设置重复和包裹模式，可以让纹理在超出范围时重复或者镜像。
-
-// 重复
-// texture.repeat.x = 2
-// texture.repeat.y = 3
-// S代表水平轴，T代表垂直轴，设置纹理在水平和垂直方向上的包裹模式为重复
-// texture.wrapS = THREE.RepeatWrapping
-// texture.wrapT = THREE.RepeatWrapping
-// 镜像 
-// texture.wrapS = THREE.MirroredRepeatWrapping
-// texture.wrapT = THREE.MirroredRepeatWrapping
-// 设置纹理偏移，默认情况下，纹理坐标的原点在左下角，通过设置偏移可以改变纹理坐标的原点位置。
-// texture.offset.x = 0.5
-// texture.offset.y = 0.5
-// 设置纹理旋转，默认情况下，纹理坐标的旋转中心在左下角，通过设置旋转可以改变纹理坐标的旋转中心位置。
-// texture.rotation = Math.PI / 4
-// texture.center.x = 0.5
-// texture.center.y = 0.5
-
-// 设置纹理过滤，默认情况下，纹理在缩小或者放大时会使用线性过滤，这会导致纹理模糊，
-// 通过设置纹理过滤可以让纹理在缩小或者放大时使用最近点过滤，这样可以保持纹理的清晰度。(反正就是保证纹理图的清晰度 )
-texture.minFilter = THREE.NearestFilter
-texture.magFilter = THREE.NearestFilter
-// 关闭纹理的mipmap生成，mipmap是一种多级渐远纹理，可以在不同距离下使用不同分辨率的纹理来提高性能和视觉效果，
-// 但是对于像素风格的纹理来说，mipmap会导致模糊，所以需要关闭。关闭了也会对GPU好点 
-texture.generateMipmaps = false
-
-=======
- *  Textures
- */
-const image = new Image()
-image.onload = () => {
-  console.log('image loaded')
-}
-
-image.src = '/textures/door/color.jpg'
-
-// const textureLoader = new THREE.TextureLoader()
-
->>>>>>> 6c8f56a62c8023b66502ff5d6483e474a441e2b4
 window.addEventListener('resize', () => {
   // 更新尺寸
   size.width = window.innerWidth
@@ -109,28 +22,99 @@ window.addEventListener('resize', () => {
   renderer.setSize(size.width, size.height)
 })
 
+
+// 创建纹理
+// const texture = new THREE.TextureLoader()
+// const doorColorTexture = texture.load('/src/static/textures/door/color.jpg')
+// const doorAlphaTexture = texture.load('/src/static/textures/door/alpha.jpg')
+// const doorHeightTexture = texture.load('/src/static/textures/door/height.jpg')
+// const doorNormalTexture = texture.load('/src/static/textures/door/normal.jpg')
+// const doorAmbientOcclusionTexture = texture.load('/src/static/textures/door/ambientOcclusion.jpg')
+// const doorMetalnessTexture = texture.load('/src/static/textures/door/metalness.jpg')
+// const doorRoughnessTexture = texture.load('/src/static/textures/door/roughness.jpg')
+// const matcapTexture = texture.load('/src/static/textures/matcaps/3.png')
+// const gradientTexture = texture.load('/src/static/textures/gradients/3.jpg')
+
 // 创建场景
 const scene = new THREE.Scene()
 
 // 创建正方体
-const geometry = new THREE.BoxGeometry(1, 1, 1) 
-console.log(geometry.attributes.uv )
-// 创建球体
-// const geometry = new THREE.SphereGeometry(1, 32, 32)
-// 创建圆锥
-// const geometry = new THREE.ConeGeometry(1, 2, 32)
-// 创建环面
-// const geometry = new THREE.TorusGeometry(1, 0.4, 16, 100)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
 
-// const material = new THREE.MeshBasicMaterial({  color: 0xff0000 })
-const material = new THREE.MeshBasicMaterial({  map: texture })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+
+// 材质
+// const material = new THREE.MeshBasicMaterial({})
+// material.map = doorColorTexture
+// material.color = new THREE.Color('purple')
+// material.wireframe = true
+// material.opacity = 0.5
+
+// 设置透明度，0.5 就是半透明，灰色。1是完全不透明,白色。
+// 0是完全透明，黑色。
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+// 显示两面
+// material.side = THREE.DoubleSide
+
+// 这个材质会根据光照的情况显示不同的颜色，
+// 适合做金属或者塑料材质
+// 一般用于调试法线
+// const material = new THREE.MeshNormalMaterial()
+// material.wireframe = true
+
+// 网格材质球-可以在无光照前提下模拟光照下的阴影等。
+// https://github.com/nidorx/matcaps 可下载材质球
+// const material = new THREE.MeshMatcapMaterial()
+// material.matcap = matcapTexture
+
+// 网格深度材质
+// 隔远了什么都看不到，拉近了可以看到灰色/白色，适用于做大雾效果
+// const material = new THREE.MeshDepthMaterial()
+
+// 受光照影响。Lambert：无高光，漫反射，哑光效果
+// 适合做：墙壁、石头、砖块、布料、木头、哑光物体。
+// const material = new THREE.MeshLambertMaterial()
+
+// MeshPhong。Phong：有高光，反光效果，亮面 / 光滑效果。
+// 类似上面，但是没有光线反射。性能不如上面那个。
+// 适合做：塑料玩具、陶瓷、亮面物体、油漆、玻璃框、门、金属（简单金属）。
+// const material = new THREE.MeshPhongMaterial()
+// // 材质光滑度
+// material.shininess = 100
+// // 特定颜色的光线反射
+// material.specular = new THREE.Color(0x1188ff)
+
+// 网格材质(默认为卡通)
+const material = new THREE.MeshToonMaterial()
+
+// 创建球体
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material)
+// 创建平面
+const plane = new THREE.Mesh( new THREE.PlaneGeometry(1, 1), material)
+plane.position.x = 1.5
+// 创建圆环
+const torus = new THREE.Mesh(new THREE.TorusGeometry(0.3, 0.2, 16, 32), material)
+torus.position.x = -1.5
+// scene.add(sphere)
+// scene.add(plane)
+scene.add(sphere, plane, torus)
 
 // 创建相机
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height)
 camera.position.z = 3
 scene.add(camera)
+
+// 创建灯光
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 100)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
 
 // 控制器
 const controls = new OrbitControls(camera, canvas)
@@ -144,6 +128,14 @@ renderer.setSize(size.width, size.height)
 
 // Animations
 const tick = () => {
+
+  const elapsedTime = clock.getElapsedTime()
+
+  // update objects
+  sphere.rotation.y = 0.1 * elapsedTime
+  plane.rotation.y = 0.1 * elapsedTime
+  torus.rotation.y = 0.1 * elapsedTime
+
   // 更新控制器（这里是加了阻尼效果）
   controls.update()
 
